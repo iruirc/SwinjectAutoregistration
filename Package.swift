1b0ad43115730d009ea251917cf2a1806977559d
Dmitry Nuzhin <iruirc@gmail.com>
2024-06-26 14:09:50 +0300

    Change dependency to 'Swinject-Dynamic' from Swinject

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         .target(
             name: "SwinjectAutoregistration",
             dependencies: [
-                "Swinject",
+                .product(name: "Swinject-Dynamic", package: "Swinject")
             ],
             path: "Sources"),
     ]
